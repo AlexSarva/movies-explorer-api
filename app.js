c5169068f0402a209378c34a8e03a080806f9a96
alexsarva <alexsarva@gmail.com>
2023-01-17 18:14:05 +0300

    fix: В production-режиме адрес базы данных берётся из process.env

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const app = require('./routes/index');
 
-const { PORT } = process.env;
+const { PORT, DATABASE_URL, NODE_ENV } = process.env;
+const DB_URL = NODE_ENV === 'production' ? DATABASE_URL : '127.0.0.1:27017/bitfilmsdb';
 
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+mongoose.connect(`mongodb://${DB_URL}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
